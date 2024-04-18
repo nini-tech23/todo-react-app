0f2f1a2f430431f4aeea0a246061960eaea7173c
lkhagvadulam <gg.lkhagvadulam@gmail.com>
2024-04-18 08:43:55 +0800

    Set input text with the default value after add task

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,21 +1,22 @@
 import classes from "../index.module.css";
-const NewTask = ({ onAdd, newTaskText, enteredTask}) => {
+const NewTask = ({ onAdd, newTaskText, onValue }) => {
     return (
-            <form
-                className={classes.addtask}
-                onSubmit={onAdd}>
-                <input
-                    type="text"
-                    placeholder="What is the task today?"
-                    className={classes.taskbody}
-                    onChange={newTaskText}
-                />
-                <button
-                    className={classes.submitBtn}
-                    type="submit">
-                    Add Task
-                </button>
-            </form>
+        <form
+            className={classes.addtask}
+            onSubmit={onAdd}>
+            <input
+                type="text"
+                placeholder="What is the task today?"
+                className={classes.taskbody}
+                onChange={newTaskText}
+                value={onValue}
+            />
+            <button
+                className={classes.submitBtn}
+                type="submit">
+                Add Task
+            </button>
+        </form>
     );
 };
 export default NewTask;
